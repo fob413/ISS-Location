8a9a5d74477438bbf41f7a363ded434da157e2a7
fob1493 <fob1493@gmail.com>
2018-10-17 11:43:43 +0100

    chore(setup): test
    - setup the tests for the server
    - setup coverage
    - setup continuous integration

---
+++ server/test/main_test.js
@@ -0,0 +1,10 @@
+// var expect = require('chai').expect;
+import { expect } from 'chai';
+
+// Test suite
+describe('ISSLocation',  () => {
+	// Test spec (unit test)
+	it('should run tests',  () => {
+		expect(true).to.be.ok;
+	});
+});
