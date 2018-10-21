04e6ee598ef1f4aded4f1f6a0897f7152c8b67e0
fob1493 <fob1493@gmail.com>
2018-10-21 18:10:14 +0100

    feat(frontend):
    - design the basic layout of the homepage

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { MapComponent } from './map/map.component';
+import { AppPageComponent } from './app-page/app-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    MapComponent,
+    AppPageComponent
   ],
   imports: [
     BrowserModule,
